347541f5b201b9f3a15992a2cdb13baf196b2509
David Rebolleda Mir <david@MBP-de-David.lan>
2018-06-20 23:46:41 +0200

    feat: add random dog images

--- controllers/pet.controller.js
+++ controllers/pet.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const PetModel = require('../models/pet');
 const OrganizationModel = require('../models/org');
+global.fetch = require('node-fetch');
 
 exports.getPets = async (ctx, next) => {
   try {
@@ -29,6 +30,12 @@ exports.addPet = async (ctx, next) => {
   try {
     console.log('=======>>>>> addPet ctx.request.body',ctx.request.body);
     const org_id = ctx.request.body.organization;
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json();
+    console.log('log before');
+    console.log('data',data);
+    console.log('log something');
+    ctx.request.body.image = data.message;
     const newPet = new PetModel(ctx.request.body);
     newPet.save()
     const org = await OrganizationModel.findById(org_id);
